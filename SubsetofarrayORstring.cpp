8421834ddaa686c0bca3c3b24e4a4c71b525eca4
Rushikesh Taskar <84194293+Rushikesh-Taskar@users.noreply.github.com>
2022-02-07 12:32:29 +0530

    Update SubsetofarrayORstring.cpp

--- SubsetofarrayORstring.cpp
+++ SubsetofarrayORstring.cpp
@@ -22,3 +22,34 @@ public:
         return ans;
     }
 };
+
+// if duplicate element present then result should be unique for that use set 
+class Solution {
+public:
+    set<vector<int>> ans;  //diffrent
+    void allsubset(vector<int> partialresult ,vector<int> nums)
+    {
+        if(nums.size()==0)
+        {
+            ans.insert(partialresult);  //diffrent
+            return;
+        }
+        
+        int a=nums[0];
+        nums.erase(nums.begin());
+        
+        allsubset(partialresult,nums);
+        
+        partialresult.push_back(a);
+        
+        allsubset(partialresult,nums);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+        vector<int>partialresult;
+        sort(nums.begin(),nums.end());  // diffrent
+        allsubset(partialresult,nums);
+        vector<vector<int>> ans1(ans.begin(),ans.end());  //diffrent
+        return ans1;
+    }
+};
