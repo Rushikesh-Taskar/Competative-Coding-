b23b9739a5240b9393d369b9754c0aaf68292f38
Rushikesh Taskar <84194293+Rushikesh-Taskar@users.noreply.github.com>
2022-04-07 18:28:58 +0530

    Update dponStock.cpp

--- dponStock.cpp
+++ dponStock.cpp
@@ -1,4 +1,42 @@
- vector<vector<int>> dp(price.size()+1 , vector<int>(2,0));  
+// memoization 
+class Solution {
+public:
+    int findmax(vector<int>& price ,int i,int buy ,vector<vector<int>>& dp)
+    {
+        
+        if(i==price.size())  // base case
+        {
+            return 0;
+        }
+        
+        if(dp[i][buy]!=-1)
+        {
+            return dp[i][buy];
+        }
+        if(buy) // buy condition 
+        {
+             return dp[i][buy]= max(-price[i]+findmax(price,i+1 ,0 ,dp), findmax(price,i+1,1,dp)); 
+            //                     buy                         not buy
+        }
+        else
+        {
+          return dp[i][buy] = max(price[i]+findmax(price,i+1 ,1 ,dp) , findmax(price,i+1,0 ,dp));
+                             //   sell                     not sell 
+        }
+        
+        
+    }
+    int maxProfit(vector<int>& prices) 
+    {
+        vector<vector<int>> dp(prices.size() , vector<int>(2,-1));  // dp for memoization 
+        return findmax(prices ,0,1 ,dp);   // start buy or not 
+    }
+};
+
+
+
+// tabulation 
+vector<vector<int>> dp(price.size()+1 , vector<int>(2,0));  
         int n= price.size();
       
         for(int i=n-1;i>=0;i--)
