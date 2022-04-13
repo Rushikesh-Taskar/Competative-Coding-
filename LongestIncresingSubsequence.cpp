54a50bf58d84a3fa5c4699d390ed28c4d828fad6
Rushikesh Taskar <84194293+Rushikesh-Taskar@users.noreply.github.com>
2022-04-13 19:10:18 +0530

    Update LongestIncresingSubsequence.cpp

--- LongestIncresingSubsequence.cpp
+++ LongestIncresingSubsequence.cpp
@@ -53,6 +53,32 @@ public:
 };
 
 
+// tabulation shift index approach 
+        int n=nums.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            for(int prev=ind-1;prev>=-1;prev--)
+            {
+                  int len = dp[ind+1][prev+1];
+        
+                  if(prev==-1 || nums[ind]>nums[prev])
+                  {
+          
+                      len = max(len , 1+dp[ind+1][ind+1]);
+           
+                  }
+        
+                 dp[ind][prev+1] =len;  // shift index 
+                
+                
+            }
+        }
+        
+        return dp[0][-1+1];
+
 
 
 
